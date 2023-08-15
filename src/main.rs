02b35eeb013daca147f03e4ddbe4895e84f3c692
Hollanti, Juha <juha.hollanti@wartsila.com>
2023-08-15 17:07:04 +0300

    clean up

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 use audioviz::io::{Input, Device};
-use audioviz::spectrum::{Frequency, config::{StreamConfig, ProcessorConfig, Interpolation}, stream::Stream};
-use std::{thread, time};
+use audioviz::spectrum::{config::{StreamConfig}, stream::Stream};
 use std::io::{Write, stdout};
 use crossterm::{QueueableCommand, cursor, terminal, ExecutableCommand};
 
 fn main() {
     // captures audio from system using cpal
     let mut audio_input = Input::new();
-    let (channel_count, sampling_rate, input_controller) = audio_input.init(&Device::DefaultInput).unwrap();
+    let (_, _, input_controller) = audio_input.init(&Device::DefaultInput).unwrap();
 
     // initialize console output
     let mut stdout = stdout();
@@ -21,32 +20,31 @@ fn main() {
             stream.update();
         }
 
+        let mut hvol :f32 = 0.0;
+        let mut highest :f32 = 0.0;
+
         let frequencies = stream.get_frequencies();
-        for (i, frequency) in frequencies.iter().enumerate() {
-            //println!("{}: {:?}", i, frequency);
-            let mut hvol :f32 = 0.0;
-            let mut highest :f32 = 0.0;
+        for (_, frequency) in frequencies.iter().enumerate() {
+            //println!("{}: {:?}", i, frequency);    
             for item in frequency {
                 if item.volume > hvol {
                     hvol = item.volume;
                     highest = item.freq;
                 }
             }
-            
-            stdout.queue(cursor::SavePosition).unwrap();
-            stdout.write_all(format!("Frequency: {} ", highest).as_bytes()).unwrap();
-            stdout.queue(cursor::RestorePosition).unwrap();
-            stdout.flush().unwrap();
-            stdout.queue(cursor::RestorePosition).unwrap();
-            stdout.queue(terminal::Clear(terminal::ClearType::FromCursorDown)).unwrap();
         }
+
+        stdout.queue(cursor::SavePosition).unwrap();
+        stdout.write_all(format!("Frequency: {} ", highest).as_bytes()).unwrap();
+        stdout.queue(cursor::RestorePosition).unwrap();
+        stdout.flush().unwrap();
+        stdout.queue(cursor::RestorePosition).unwrap();
+        stdout.queue(terminal::Clear(terminal::ClearType::FromCursorDown)).unwrap();
         
         //println!("Frequencies: {:?}", frequencies);
-
         //break;
     }
     
-    stdout.execute(cursor::Show).unwrap();
-    println!("Done!");
-
+    //stdout.execute(cursor::Show).unwrap();
+    //println!("Done!");
 }
