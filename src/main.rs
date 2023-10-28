205866a26990cbeec9732b674d41b4505918fe11
Hollanti, Juha <juha.hollanti@wartsila.com>
2023-10-28 17:39:39 +0300

    add back original naive fft pitch detector

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ use serde::{Deserialize, Serialize};
 use pitch_detection::detector::mcleod::McLeodDetector;
 use pitch_detection::detector::PitchDetector;
 
+use audioviz::spectrum::{config::{StreamConfig as StreamConfig2, ProcessorConfig, VolumeNormalisation, PositionNormalisation, Interpolation}, stream::Stream};
+
 #[derive(Debug, Serialize, Deserialize)]
 struct Config {
     device_id: usize,
@@ -71,6 +73,57 @@ impl PitchFindTrait for McleodPitchDetector {
     }
 }
 
+struct FftPitchDetector {
+    stream: Stream,
+}
+
+impl FftPitchDetector {
+    fn new() -> FftPitchDetector {
+        // spectrum visualizer stream
+        let stream: Stream = Stream::new(StreamConfig2 {
+            channel_count: 1,
+            processor: ProcessorConfig {
+                sampling_rate: 8192,
+                frequency_bounds: [0, 1000],
+                resolution: None,
+                volume: 1.0,
+                volume_normalisation: VolumeNormalisation::Mixture,
+                position_normalisation: PositionNormalisation::Harmonic,
+                manual_position_distribution: None,
+                interpolation: Interpolation::Cubic,
+            },
+            fft_resolution: 1024,
+            refresh_rate: 30,
+            gravity: Some(5.0),
+        });
+
+        FftPitchDetector {stream}
+    }
+}
+
+impl PitchFindTrait for FftPitchDetector {
+    fn maybe_find_pitch(&mut self, data: &[f64]) -> Option<f64> {
+        let vec: Vec<f32> = data.iter().map(|&x| x as f32).collect();
+
+        self.stream.push_data(vec);
+        self.stream.update();
+        
+        let mut hvol :f32 = 0.0;
+        let mut highest :f32 = 0.0;
+
+        let frequencies = self.stream.get_frequencies();
+        for (_, frequency) in frequencies.iter().enumerate() {
+            for item in frequency {
+                if item.volume > hvol {
+                    hvol = item.volume;
+                    highest = item.freq;
+                }
+            }
+        }
+        return Some(highest as f64);
+    }
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // read config.cfg
     let f = std::fs::File::open("config.yaml")?;
@@ -112,6 +165,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 config.clarity_threshold);
             detector = Box::new(mcleod);
         }
+        "fft" => {
+            let fft = FftPitchDetector::new();
+            detector = Box::new(fft);
+        }
         _ => panic!("Invalid pitch detection method"),
     };
     
@@ -150,7 +207,6 @@ fn output(freq:f64) {
     let mut stdout = stdout();
     stdout.execute(cursor::Hide).unwrap();
     stdout.queue(cursor::SavePosition).unwrap();
-    // times two because channels = 2
     stdout.write_all(format!("Frequency: {} ", freq).as_bytes()).unwrap();
     stdout.queue(cursor::RestorePosition).unwrap();
     stdout.flush().unwrap();
