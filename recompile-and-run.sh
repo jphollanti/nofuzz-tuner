5ab23b294955acc87519d5d4fc91a561d56eb449
pikkuaapo <pikkuaapo@gmail.com>
2024-10-13 12:25:29 +0300

    + recompile and run svelte app

--- recompile-and-run.sh
+++ recompile-and-run.sh
@@ -1,4 +1,10 @@
 pushd nofuzz_tuner_lib
 wasm-pack build --release --target web
 popd
-python -m http.server 
+rm -rf nofuzz-tuner-frontend/src/lib/no_fuzz_tuner
+mkdir -p nofuzz-tuner-frontend/src/lib/no_fuzz_tuner/pkg
+cp -r nofuzz_tuner_lib/pkg nofuzz-tuner-frontend/src/lib/no_fuzz_tuner
+
+pushd nofuzz-tuner-frontend
+npm run dev    
+popd
